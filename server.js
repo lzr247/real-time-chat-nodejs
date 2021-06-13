6cf893fc6643e2de870713b6507d140a43394c25
lzr247 <lazarconic@yahoo.com>
2021-06-13 11:43:39 +0200

    some socketio events

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when a client connects
+io.on('connection', socket => {
+
+    // Welcome current user
+    socket.emit('message', 'Welcome to Node Chat!');
+
+    // Broadcast when a user connects
+    // Send to everyone except the user thats connnecting
+    socket.broadcast.emit('message', 'A user has joined the chat.');
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat.');
+    })
+
+    // Broadcast to everybody
+    // io.emit();
+
+})
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
